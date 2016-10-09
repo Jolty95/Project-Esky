46115d440170ccfd3906f9e715262c5515862843
Jolty <juplagon@gmail.com>
2016-10-09 21:17:03 +0200

    Intento de encontrar el offset nº2

--- sources/index.lua
+++ sources/index.lua
@@ -41,20 +41,30 @@ while true do
 			existeArchivo = System.doesFileExist(ruta)
 			--Si no existe el archivo, lo creamos
 			if existeArchivo ~= true then
-			
+				--creamos aqui
 			else
 				if c == 0 then
 					local file = io.open(ruta, FWRITE)
-					local oldIP = io.read(file, 83, io.size(file))
+					local oldIP = io.read(file, 87, io.size(file))
+					--local puerto = io.read(file, 103, io.size(file))
 					io.close(file)
-					local test = io.open("/test.txt",FCREATE) 
-					io.write(test, 0, oldIP, 100)
-					io.close(test)
+					--local test = io.open("/test.txt",FCREATE) 
+					--io.write(test, 0, puerto, 100)
+					--io.close(test)
+					--local i --indice empieza puerto
+					--local j --indice final puerto
+					
+					--i, j = string.find(oldIP, "Port: "...puerto)
+					--local subString = 
 					Screen.refresh()
 					Screen.debugPrint(5,15, "IP: "..oldIP, Color.new(255,0,0), TOP_SCREEN)
-					Screen.waitVblankStart()
+					--Screen.debugPrint(5,15, "IP: "..subString, Color.new(255,0,0), BOTTOM_SCREEN)
+					--Screen.debugPrint(20, 20, "Puerto: "..puerto, Color.new(255,0,0), TOP_SCREEN)
+					Screen.waitVblankStart()		
 					c = 1
 				end
+				
+				
 			end			
 		end			
 	end	
