fc8620d00a94cc431d75cf64906d7da41e37980e
Jolty <juplagon@gmail.com>
2016-10-10 15:16:37 +0200

    Revert "Index.lua a7030cb"

--- sources/index.lua
+++ sources/index.lua
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
---** Código fuente del Project: Esky
+--** Código fuente del Proyect: Esky
 -- **********************************
 --
 -- Archivo index.lua
@@ -42,33 +41,21 @@ while true do
 			existeArchivo = System.doesFileExist(ruta)
 			--Si no existe el archivo, lo creamos
 			if existeArchivo ~= true then
-				--creamos aqui
+			
 			else
 				if c == 0 then
 					local file = io.open(ruta, FWRITE)
-					local oldIP = io.read(file, 87, io.size(file))
-					--local puerto = io.read(file, 103, io.size(file))
+					local oldIP = io.read(file, 83, io.size(file))
 					io.close(file)
-					--local test = io.open("/test.txt",FCREATE) 
-					--io.write(test, 0, puerto, 100)
-					--io.close(test)
-					--local i --indice empieza puerto
-					--local j --indice final puerto
-					
-					--i, j = string.find(oldIP, "Port: "...puerto)
-					--local subString = 
+					local test = io.open("/test.txt",FCREATE) 
+					io.write(test, 0, oldIP, 100)
+					io.close(test)
 					Screen.refresh()
 					Screen.debugPrint(5,15, "IP: "..oldIP, Color.new(255,0,0), TOP_SCREEN)
-					--Screen.debugPrint(5,15, "IP: "..subString, Color.new(255,0,0), BOTTOM_SCREEN)
-					--Screen.debugPrint(20, 20, "Puerto: "..puerto, Color.new(255,0,0), TOP_SCREEN)
-					Screen.waitVblankStart()		
+					Screen.waitVblankStart()
 					c = 1
 				end
-				
-				
 			end			
 		end			
 	end	
 end
-=======
->>>>>>> parent of a7030cb... Intentando localizar el offset de la posicion de la IP
